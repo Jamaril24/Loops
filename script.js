7e52107e8647a75afa23077214bccfda5414f0c6
Jamaril24 <jamarlopez24@g.crookcountyschools.org>
2021-11-04 09:37:20 -0700

    complete

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log ('while loop 1')
+
+function while1 (){
+    let i = 0
+        while (i<= 10){
+         console. log (i*2+1)
+         i++;
+    } 
+}
+while1()
+
+console. log ('while loop 2')
+
+function while2 () {
+    let i = 0
+         while (i <= 20){
+            console. log (i*5)
+            i++; 
+     }
+}
+while2 ()
+
+console. log ('For Loop 1')
+
+function for1 (){
+    for (let i = 0; i <= 20; i++) {
+        console. log (i*4);
+        
+    }
+}
+for1 ()
+
+console. log ('For Loop 2')
+
+function for2 () {
+    for (let i = 0; i <= 30; i++) {
+        console.log (i*3);
+        
+    }
+}
+for2 ()
